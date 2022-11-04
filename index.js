1987a9e6180c941cb76368f65941f4847e85dbbc
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2022-11-04 15:51:53 +0530

    ice cream

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const redux= require('redux')
+const reduxLogger= require('redux-logger')
 const createStore=redux.createStore
-
+const combineReducers= redux.combineReducers;
+const logger=reduxLogger.createLogger(P);
+const applyMiddleware=redux.applyMiddleware;
 const BUY_CAKE='BUY_CAKE'
-
+const BUY_ICE='BUY_ICE'
 function buyCake(){
     return{
         type: BUY_CAKE,
@@ -10,11 +13,23 @@ function buyCake(){
     }
 }
 
-const initialState={
-    noOfCakes:50
+function buyIce(){
+    return{
+        type: BUY_ICE,
+        info:' First ICE redux'
+    }
+}
+const initialCake={
+    noOfCakes:50,
 }
 
-const reducer=(state=initialState, action)=>{
+const initialIce={
+    
+    noOfIce:10,
+}
+
+
+const cakeReducer=(state=initialCake, action)=>{
     switch(action.type){
         case 'BUY_CAKE':
             return{
@@ -22,16 +37,38 @@ const reducer=(state=initialState, action)=>{
                 noOfCakes:state.noOfCakes + 1,
                // noOfCakes:state.noOfCakes*4
             }
+           
         default:
             return state
     }
 }
 
-const store=createStore(reducer)
+const iceReducer=(state=initialIce, action)=>{
+    switch(action.type){
+        case 'BUY_ICE':
+            return{
+                ...state,
+                noOfIce:state.noOfIce+ 1,
+               // noOfCakes:state.noOfCakes*4
+            }
+           
+        default:
+            return state
+    }
+}
+
+const rootReducer=combineReducers({
+    cake: cakeReducer,
+    ice: iceReducer
+})
+
+const store=createStore(rootReducer,applyMiddleware(logger))
 console.log('initial state', store.getState())
-store.subscribe(()=> console.log('update state', store.getState()))
+store.subscribe(()=>{})
 
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIce())
+store.dispatch(buyIce())
 
